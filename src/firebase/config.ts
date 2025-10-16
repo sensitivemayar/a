db69c767c688b1119b01e6825e69e341979433b9
Sahana Parival <parivalsahana@gmail.com>
2025-10-16 17:23:40 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1691155405-c2f4e",
+  "appId": "1:678011897071:web:19d1e32a3053a29785471a",
+  "apiKey": "AIzaSyBGczXqcGhKkxBxBPbwERPIjkwBFZ2Clzg",
+  "authDomain": "studio-1691155405-c2f4e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "678011897071"
+};
